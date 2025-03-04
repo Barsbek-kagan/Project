1bc7f626cd832a067ea784fdb63c027e5c2405c9
Barsbek <beishekanovbarsbek@gmail.com>
2025-03-04 19:49:30 +0600

    Initial commit

---
+++ App.jsx
@@ -0,0 +1,16 @@
+
+
+// App.jsx
+import React from 'react';
+import Counter from './components/Counter';
+
+function App() {
+    return (
+        <div>
+            <h1>Counter App</h1>
+
+        </div>
+    );
+}
+
+export default App;
---
+++ components/Counter.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './counterSlice.jsx';
+
+export default function Counter() {
+    const count = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <button onClick={() => dispatch(decrement())}>-</button>
+            <span>{count}</span>
+            <button onClick={() => dispatch(increment())}>+</button>
+        </div>
+    );
+}
